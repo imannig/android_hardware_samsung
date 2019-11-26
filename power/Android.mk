6486dbdfb19340644f4ad869d4cad3775a3cda51
Jan Altensen <info@stricted.net>
2019-11-26 20:34:28 +0100

    samsung: power: dont depend on liblights_helper
     * this is partially a revert of 1226350ee7101a4dc42a4903e148f17c4bd54e4b
    
    Change-Id: I0ab7f6dc7642f7778f802e18b2f2dda7c11d9b3b
    (cherry picked from commit 39e99623e0f5040ab002502c130a0858d985f73f)
    
    # Conflicts:
    #	power/Android.mk

--- power/Android.mk
+++ power/Android.mk
@@ -20,21 +20,15 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := power.c
 
-LOCAL_C_INCLUDES += \
-	$(LOCAL_PATH)/include \
-	hardware/libhardware/include
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include \
+    $(LOCAL_PATH)/../hidl/light/include
 
-LOCAL_SHARED_LIBRARIES := \
-	liblog \
-    libcutils \
-	libhardware
-
-LOCAL_STATIC_LIBRARIES := liblights_helper
+LOCAL_SHARED_LIBRARIES := liblog libcutils
 
 LOCAL_MODULE := power.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS := -DTARGET_TAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
