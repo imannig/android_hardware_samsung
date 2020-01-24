bc8b37e0d7009bc3306af6562fcd10e813a21935
Astrako <apamarin@gmail.com>
2020-01-24 08:15:34 +0100

    Fix some qcom bluetooth oss stuff

--- qcom-bt/include/hw_rome.h
+++ qcom-bt/include/hw_rome.h
@@ -151,17 +151,21 @@
 #define EXTRACT_BYTE(val, pos)      (char) (((val) >> (8 * (pos))) & 0xFF)
 #define CALC_SEG_SIZE(len, max)   ((plen) % (max))?((plen/max)+1) : ((plen) / (max))
 
-#define ROME_FW_PATH                      "/vendor/firmware/rampatch.img"
-#define ROME_RAMPATCH_TLV_PATH            "/vendor/firmware/rampatch_tlv.img"
-#define ROME_NVM_TLV_PATH                 "/vendor/firmware/nvm_tlv.bin"
-#define ROME_RAMPATCH_TLV_1_0_3_PATH      "/vendor/firmware/rampatch_tlv_1.3.tlv"
-#define ROME_NVM_TLV_1_0_3_PATH           "/vendor/firmware/nvm_tlv_1.3.bin"
-#define ROME_RAMPATCH_TLV_2_0_1_PATH      "/vendor/firmware/rampatch_tlv_2.1.tlv"
-#define ROME_NVM_TLV_2_0_1_PATH           "/vendor/firmware/nvm_tlv_2.1.bin"
-#define ROME_RAMPATCH_TLV_3_0_0_PATH      "/bt_firmware/image/btfw30.tlv"
-#define ROME_NVM_TLV_3_0_0_PATH           "/bt_firmware/image/btnv30.bin"
-#define ROME_RAMPATCH_TLV_3_0_2_PATH      "/bt_firmware/image/btfw32.tlv"
-#define ROME_NVM_TLV_3_0_2_PATH           "/bt_firmware/image/btnv32.bin"
+#define ROME_FW_PATH                    "/vendor/firmware/rampatch.img"
+#define ROME_RAMPATCH_TLV_PATH          "/vendor/firmware/rampatch_tlv.img"
+#define ROME_NVM_TLV_PATH               "/vendor/firmware/nvm_tlv.bin"
+#define ROME_RAMPATCH_TLV_1_0_3_PATH    "/vendor/firmware/rampatch_tlv_1.3.tlv"
+#define ROME_NVM_TLV_1_0_3_PATH         "/vendor/firmware/nvm_tlv_1.3.bin"
+#define ROME_RAMPATCH_TLV_2_0_1_PATH    "/vendor/firmware/rampatch_tlv_2.1.tlv"
+#define ROME_NVM_TLV_2_0_1_PATH         "/vendor/firmware/nvm_tlv_2.1.bin"
+#define ROME_RAMPATCH_TLV_3_0_0_PATH    "/vendor/firmware/btfw30.tlv"
+#define ROME_NVM_TLV_3_0_0_PATH         "/vendor/firmware/btnv30.bin"
+#define ROME_RAMPATCH_TLV_3_0_2_PATH    "/vendor/firmware/btfw32.tlv"
+#define ROME_NVM_TLV_3_0_2_PATH         "/vendor/firmware/btnv32.bin"
+#define TF_RAMPATCH_TLV_1_0_0_PATH      "/vendor/firmware/rampatch_tlv_tf_1.0.tlv"
+#define TF_NVM_TLV_1_0_0_PATH           "/vendor/firmware/nvm_tlv_tf_1.0.bin"
+#define TF_RAMPATCH_TLV_1_0_1_PATH      "/vendor/firmware/rampatch_tlv_tf_1.1.tlv"
+#define TF_NVM_TLV_1_0_1_PATH           "/vendor/firmware/nvm_tlv_tf_1.1.bin"
 
 #define ROME_3_1_FW_SU  "bprm.cnss.3.1"
 #define ROME_3_2_FW_SU  "btfwp.cnss.3.2"
@@ -267,7 +271,9 @@ enum{
 enum{
     ROME_SOC_ID_00 = 0x00000000,
     ROME_SOC_ID_11 = 0x00000011,
+    ROME_SOC_ID_13 = 0x00000013,
     ROME_SOC_ID_22 = 0x00000022,
+    ROME_SOC_ID_23 = 0x00000023,
     ROME_SOC_ID_44 = 0x00000044
 };
 
@@ -278,7 +284,9 @@ enum{
     ROME_VER_1_3 = ((ROME_PATCH_VER_0200 << 16 ) | ROME_SOC_ID_00 ),
     ROME_VER_2_1 = ((ROME_PATCH_VER_0200 << 16 ) | ROME_SOC_ID_11 ),
     ROME_VER_3_0 = ((ROME_PATCH_VER_0300 << 16 ) | ROME_SOC_ID_22 ),
-    ROME_VER_3_2 = ((ROME_PATCH_VER_0302 << 16 ) | ROME_SOC_ID_44 )
+    ROME_VER_3_2 = ((ROME_PATCH_VER_0302 << 16 ) | ROME_SOC_ID_44 ),
+    TUFELLO_VER_1_0 = ((ROME_PATCH_VER_0300 << 16 ) | ROME_SOC_ID_13 ),
+    TUFELLO_VER_1_1 = ((ROME_PATCH_VER_0302 << 16 ) | ROME_SOC_ID_23 )
 };
 
 //declarations
--- qcom-bt/src/hw_rome.c
+++ qcom-bt/src/hw_rome.c
@@ -95,6 +95,8 @@ int dnld_fd = -1;
 /*****************************************************************************
 **   Functions
 *****************************************************************************/
+int userial_vendor_set_hw_flow_control(userial_vendor_ioctl_op_t op);
+
 int do_write(int fd, unsigned char *buf,int len)
 {
     int ret = 0;
@@ -1460,6 +1462,10 @@ int rome_set_baudrate_req(int fd)
     /* Total length of the packet to be sent to the Controller */
     size = (HCI_CMD_IND + HCI_COMMAND_HDR_SIZE + VSC_SET_BAUDRATE_REQ_LEN);
 
+    /* HW flow off */
+    userial_vendor_set_hw_flow_control(USERIAL_OP_FLOW_OFF);
+    tcflush(fd, TCIOFLUSH);
+
     /* Flow off during baudrate change */
     if ((err = userial_vendor_ioctl(USERIAL_OP_FLOW_OFF , &flags)) < 0)
     {
@@ -1483,6 +1489,9 @@ int rome_set_baudrate_req(int fd)
         ALOGE("%s: HW Flow-on error: 0x%x \n", __FUNCTION__, err);
         return err;
     }
+
+    /* HW flow on */
+    userial_vendor_set_hw_flow_control(USERIAL_OP_FLOW_ON);
 
     /* Check for response from the Controller */
     if ((err =read_vs_hci_event(fd, rsp, HCI_MAX_EVENT_SIZE)) < 0) {
@@ -1525,6 +1534,9 @@ int rome_hci_reset_req(int fd)
     /* Total length of the packet to be sent to the Controller */
     size = (HCI_CMD_IND + HCI_COMMAND_HDR_SIZE);
 
+    /* HW flow off */
+    userial_vendor_set_hw_flow_control(USERIAL_OP_FLOW_OFF);
+
     /* Flow off during baudrate change */
     if ((err = userial_vendor_ioctl(USERIAL_OP_FLOW_OFF , &flags)) < 0)
     {
@@ -1549,6 +1561,9 @@ int rome_hci_reset_req(int fd)
         ALOGE("%s: HW Flow-on error: 0x%x \n", __FUNCTION__, err);
         return err;
     }
+
+    /* HW flow on */
+    userial_vendor_set_hw_flow_control(USERIAL_OP_FLOW_ON);
 
     /* Wait for command complete event */
     err = read_hci_event(fd, rsp, HCI_MAX_EVENT_SIZE);
@@ -1904,6 +1919,14 @@ int rome_soc_init(int fd, char *bdaddr)
             nvm_file_path = ROME_NVM_TLV_3_0_2_PATH;
             fw_su_info = ROME_3_2_FW_SU;
             fw_su_offset =  ROME_3_2_FW_SW_OFFSET;
+            goto download;
+        case TUFELLO_VER_1_0:
+            rampatch_file_path = TF_RAMPATCH_TLV_1_0_0_PATH;
+            nvm_file_path = TF_NVM_TLV_1_0_0_PATH;
+            goto download;
+        case TUFELLO_VER_1_1:
+            rampatch_file_path = TF_RAMPATCH_TLV_1_0_1_PATH;
+            nvm_file_path = TF_NVM_TLV_1_0_1_PATH;
 
 download:
             /* Change baud rate 115.2 kbps to 3Mbps*/
